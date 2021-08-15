7c0d6dd57e2e0f3b6d2e6b43ff91050b708aa233
Parag <paragdurafe@outlook.com>
2021-08-15 21:54:49 +0530

    webpage added

--- bmi.py
+++ bmi.py
@@ -3,9 +3,13 @@ from flask import Flask,render_template,request
 
 app=Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
+
+@app.route('/result/',methods=['POST'])
+def result():
+    return render_template('bmi_res.html')
 
 if __name__=='__main__':
     app.debug=True

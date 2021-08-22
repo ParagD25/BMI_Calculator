c6819238c1708e6ae335472410c54da8aadcc55f
Parag <paragdurafe@outlook.com>
2021-08-22 21:02:26 +0530

    emailing class added

--- mail.py
+++ mail.py
@@ -1,4 +1,16 @@
 from email.mime.text import MIMEText
+import smtplib
 
-def send_mail(email,height):
-    pass
+def send_mail(email,height,weight,user_bmi):
+    from_email=''
+    from_password=''
+    to_email=email
+
+    subject='Your BMI'
+    message="Hey there! Your BMI calculated is <strong>%s</strong>, according to your height - <strong>%s</strong> and weight - <strong>%s</strong>." %user_bmi %height %weight
+
+    msg=MIMEText(message,'html')
+    msg['Subject']=subject
+    msg['To']=to_email
+    msg['From']=from_email
+
